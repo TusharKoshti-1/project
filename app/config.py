ca66d9dfd682e972a6cd7c0db46be325de32bfa3
TusharKoshti-1 <tusharkoshti01@gmail.com>
2025-01-17 10:38:11 +0530

    ok

--- app/config.py
+++ app/config.py
@@ -3,6 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
+class OAuthConfig:
+    GOOGLE_CLIENT_ID = '754665354965-oosrn97ournnrdbv7njkubnro4li81vv.apps.googleusercontent.com'
+    GOOGLE_CLIENT_SECRET = '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'
+    GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
+
+
 DATABASE_URL = "mysql+pymysql://root:9824@localhost/productivity_monitoring"
 
 engine = create_engine(DATABASE_URL)
